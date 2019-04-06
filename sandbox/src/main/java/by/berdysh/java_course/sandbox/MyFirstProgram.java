f9dfa9fcfbfa1e08fb0c22fd54442497d14540b9
berdyshma <berdysh@gmail.com>
2019-04-06 22:15:05 +0300

    Создан класс Point (задание 2)

--- sandbox/src/main/java/by/berdysh/java_course/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/by/berdysh/java_course/sandbox/MyFirstProgram.java
@@ -2,20 +2,13 @@ package by.berdysh.java_course.sandbox;
 
 public class MyFirstProgram {
 	public static void main(String[] args) {
-		hello ("world");
-		hello ("user");
-		hello ("Maryia");
-
-		Square s = new Square(5);
-		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
-
-		Rectangle r = new Rectangle(4, 6);
-		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
-	}
-
-	public static void hello(String somebody) {
-		System.out.println("Hello " + somebody);
+		Point p = new Point(3,6,5,10);
+			System.out.println("Расстояние между точками P1 (с координатами " + p.x1 + " и " + p.x2 + " ) " +
+							"и P2 (с координатами " + p.y1 + " и " + p.y2 + " ) " + " = " + p.distance());
 	}
 
 
-}
+	}
+
+
+
