405885c64e072d8ab86f1f2ec37ae02290e74222
berdyshma <berdysh@gmail.com>
2019-04-06 18:34:49 +0300

    Создание классов Square и Rectangle

---
+++ sandbox/src/main/java/by/berdysh/java_course/sandbox/Square.java
@@ -0,0 +1,13 @@
+package by.berdysh.java_course.sandbox;
+
+public class Square {
+
+	public double l;
+
+		public Square (double l){
+		this.l = l;
+	}
+	public double area (){
+		return this.l * this.l;
+	}
+}
