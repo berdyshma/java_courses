d7d791b7177861323a1048ec7395de38ea372ddc
berdyshma <berdysh@gmail.com>
2019-04-08 18:30:38 +0300

    PointTests1

---
+++ sandbox/src/test/java/by/berdysh/java_course/sandbox/PointTests.java
@@ -0,0 +1,16 @@
+package by.berdysh.java_course.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+	@Test
+	public void testDistance (){
+
+		Point p1 = new Point(0,0);
+		Point p2 = new Point(3,4);
+		Assert.assertEquals(p1.distance(p2),5.0);
+
+	}
+}
