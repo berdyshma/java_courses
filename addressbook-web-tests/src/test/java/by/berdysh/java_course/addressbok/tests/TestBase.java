8e4da3d20011c8405695e349ba41805b13283d06
berdyshma <berdysh@gmail.com>
2019-04-19 19:42:31 +0300

    Fill forms optimization (default and existing values)

--- addressbook-web-tests/src/test/java/by/berdysh/java_course/addressbok/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/by/berdysh/java_course/addressbok/tests/TestBase.java
@@ -7,7 +7,7 @@ import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
 
-	public final ApplicationManager app = new ApplicationManager(BrowserType.IE);
+	public final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
 	@BeforeMethod(alwaysRun = true)
 	public void setUp() throws Exception {
