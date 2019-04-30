a9bb33d9b70963d8fa1366796eaa5fe3384248f7
berdyshma <berdysh@gmail.com>
2019-04-30 20:03:41 +0300

    Collection organization for Group Tests

--- addressbook-web-tests/src/test/java/by/berdysh/java_course/addressbok/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/by/berdysh/java_course/addressbok/appmanager/GroupHelper.java
@@ -3,8 +3,12 @@ package by.berdysh.java_course.addressbok.appmanager;
 import by.berdysh.java_course.addressbok.model.GroupData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-public class GroupHelper extends HelperBase{
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroupHelper extends HelperBase {
 
 	public GroupHelper(WebDriver wd) {
 		super(wd);
@@ -35,11 +39,14 @@ public class GroupHelper extends HelperBase{
 
 	public void selectGroup(int index) {
 		wd.findElements(By.name("selected[]")).get(index).click();
-		}
+	}
 
-	public void initGroupModification(){click(By.name("edit"));	}
+	public void initGroupModification() {
+		click(By.name("edit"));
+	}
 
-	public void submitGroupModification() {click(By.name("update"));
+	public void submitGroupModification() {
+		click(By.name("update"));
 	}
 
 	public void createGroup(GroupData group) {
@@ -55,5 +62,16 @@ public class GroupHelper extends HelperBase{
 
 	public int getGroupCount() {
 		return wd.findElements(By.name("selected[]")).size();
+	}
+
+	public List<GroupData> getGroupList() {
+		List<GroupData> groups = new ArrayList<GroupData>();
+		List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+		for (WebElement element : elements) {
+			String name = element.getText();
+			GroupData group = new GroupData(name, null, null);
+			groups.add(group);
+		}
+		return groups;
 	}
 }
--- addressbook-web-tests/src/test/java/by/berdysh/java_course/addressbok/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/by/berdysh/java_course/addressbok/tests/GroupCreationTests.java
@@ -4,16 +4,18 @@ import by.berdysh.java_course.addressbok.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.util.List;
+
 public class GroupCreationTests extends TestBase {
 
 
 	@Test
 	public void testGroupCreation() throws Exception {
 		app.getNavigationHelper().goToGroupPage();
-		int before = app.getGroupHelper().getGroupCount();
+		List <GroupData> before = app.getGroupHelper().getGroupList();
 		app.getGroupHelper().createGroup(new GroupData("test1", null, null));
-		int after = app.getGroupHelper().getGroupCount();
-		Assert.assertEquals(after, before + 1);
+		List <GroupData> after = app.getGroupHelper().getGroupList();
+		Assert.assertEquals(after.size(), before.size() + 1);
 		app.logOut();
 	}
 
