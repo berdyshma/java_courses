d4fcaee7a8c0b853c7857c1056459df8b6c55ff7
berdyshma <berdysh@gmail.com>
2019-04-30 23:16:47 +0300

    Unordered collections for GroupModification test

--- addressbook-web-tests/src/test/java/by/berdysh/java_course/addressbok/model/GroupData.java
+++ addressbook-web-tests/src/test/java/by/berdysh/java_course/addressbok/model/GroupData.java
@@ -3,32 +3,46 @@ package by.berdysh.java_course.addressbok.model;
 import java.util.Objects;
 
 public class GroupData {
+	public final String id;
 	public final String name;
 	public final String header;
+
+	public final String footer;
 
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) return true;
 		if (o == null || getClass() != o.getClass()) return false;
 		GroupData groupData = (GroupData) o;
-		return Objects.equals(name, groupData.name);
+		return Objects.equals(id, groupData.id) &&
+						Objects.equals(name, groupData.name);
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(name);
+		return Objects.hash(id, name);
 	}
 
 	@Override
 	public String toString() {
 		return "GroupData{" +
-						"name='" + name + '\'' +
+						"id='" + id + '\'' +
+						", name='" + name + '\'' +
 						'}';
 	}
 
-	public final String footer;
+	public String getId() {
+		return id;
+	}
 
+	public GroupData(String id, String name, String header, String footer) {
+		this.id = id;
+		this.name = name;
+		this.header = header;
+		this.footer = footer;
+	}
 	public GroupData(String name, String header, String footer) {
+		this.id = null;
 		this.name = name;
 		this.header = header;
 		this.footer = footer;
