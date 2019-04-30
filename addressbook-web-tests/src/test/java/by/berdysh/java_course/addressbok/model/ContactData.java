9bf625c8e01cd35de703caf97b73a8d1b74c92f6
berdyshma <berdysh@gmail.com>
2019-04-30 22:50:45 +0300

    Collections comparison (ContactDeletion, GroupDeletion)

--- addressbook-web-tests/src/test/java/by/berdysh/java_course/addressbok/model/ContactData.java
+++ addressbook-web-tests/src/test/java/by/berdysh/java_course/addressbok/model/ContactData.java
@@ -1,11 +1,41 @@
 package by.berdysh.java_course.addressbok.model;
 
+import java.util.Objects;
+
 public class ContactData {
 	public final String firstName;
 	public final String lastName;
 	public final String email;
 	public final String mobile;
 	private String group;
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		ContactData that = (ContactData) o;
+		return Objects.equals(firstName, that.firstName) &&
+						Objects.equals(lastName, that.lastName) &&
+						Objects.equals(email, that.email) &&
+						Objects.equals(mobile, that.mobile) &&
+						Objects.equals(group, that.group);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(firstName, lastName, email, mobile, group);
+	}
+
+	@Override
+	public String toString() {
+		return "ContactData{" +
+						"firstName='" + firstName + '\'' +
+						", lastName='" + lastName + '\'' +
+						", email='" + email + '\'' +
+						", mobile='" + mobile + '\'' +
+						", group='" + group + '\'' +
+						'}';
+	}
 
 	public ContactData(String firstName, String lastName, String email, String mobile, String group) {
 		this.firstName = firstName;
--- addressbook-web-tests/src/test/java/by/berdysh/java_course/addressbok/model/GroupData.java
+++ addressbook-web-tests/src/test/java/by/berdysh/java_course/addressbok/model/GroupData.java
@@ -1,8 +1,31 @@
 package by.berdysh.java_course.addressbok.model;
 
+import java.util.Objects;
+
 public class GroupData {
 	public final String name;
 	public final String header;
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		GroupData groupData = (GroupData) o;
+		return Objects.equals(name, groupData.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+
+	@Override
+	public String toString() {
+		return "GroupData{" +
+						"name='" + name + '\'' +
+						'}';
+	}
+
 	public final String footer;
 
 	public GroupData(String name, String header, String footer) {
