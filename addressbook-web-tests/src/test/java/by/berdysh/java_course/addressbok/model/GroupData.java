4a84fa9137d4143c3cac92d067c923b58a5a1320
berdyshma <berdysh@gmail.com>
2019-05-01 19:19:43 +0300

    Anonymous functions for GroupCreation test

--- addressbook-web-tests/src/test/java/by/berdysh/java_course/addressbok/model/GroupData.java
+++ addressbook-web-tests/src/test/java/by/berdysh/java_course/addressbok/model/GroupData.java
@@ -3,46 +3,29 @@ package by.berdysh.java_course.addressbok.model;
 import java.util.Objects;
 
 public class GroupData {
-	public final String id;
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int id;
 	public final String name;
 	public final String header;
 
 	public final String footer;
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		GroupData groupData = (GroupData) o;
-		return Objects.equals(id, groupData.id) &&
-						Objects.equals(name, groupData.name);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id, name);
-	}
-
-	@Override
-	public String toString() {
-		return "GroupData{" +
-						"id='" + id + '\'' +
-						", name='" + name + '\'' +
-						'}';
-	}
-
-	public String getId() {
+	public int getId() {
 		return id;
 	}
 
-	public GroupData(String id, String name, String header, String footer) {
+	public GroupData(int id, String name, String header, String footer) {
 		this.id = id;
 		this.name = name;
 		this.header = header;
 		this.footer = footer;
 	}
+
 	public GroupData(String name, String header, String footer) {
-		this.id = null;
+		this.id = 0;
 		this.name = name;
 		this.header = header;
 		this.footer = footer;
@@ -59,4 +42,19 @@ public class GroupData {
 	public String getFooter() {
 		return footer;
 	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		GroupData groupData = (GroupData) o;
+		return id == groupData.id &&
+						Objects.equals(name, groupData.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
 }
+
