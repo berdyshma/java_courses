96d237e4b3462cee5fb39bc5c48a20236dd597d2
berdyshma <berdysh@gmail.com>
2019-05-01 20:46:52 +0300

    Anonymous function for ContactCreation

--- addressbook-web-tests/src/test/java/by/berdysh/java_course/addressbok/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/by/berdysh/java_course/addressbok/tests/ContactCreationTest.java
@@ -1,9 +1,8 @@
 package by.berdysh.java_course.addressbok.tests;
 
 import by.berdysh.java_course.addressbok.model.ContactData;
-import by.berdysh.java_course.addressbok.model.GroupData;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 
 import java.util.HashSet;
 import java.util.List;
@@ -22,13 +21,7 @@ public class ContactCreationTest extends TestBase {
 		Assert.assertEquals(after.size(), before.size() + 1);
 
 
-    int max = 0;
-    for (ContactData c : after) {
-    	if (c.getId() > max){
-    		max = c.getId();
-			}
-		}
-    contact.setId(max);
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
 		before.add (contact);
 		Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
 
