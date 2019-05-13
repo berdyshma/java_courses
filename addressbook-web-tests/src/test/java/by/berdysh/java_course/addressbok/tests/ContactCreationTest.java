dd0d72ad69725b2ac64a2584c73cdcdf3b54eaac
berdyshma <berdysh@gmail.com>
2019-05-13 22:18:59 +0300

    Test methods parameterization for Contact and Group creation

--- addressbook-web-tests/src/test/java/by/berdysh/java_course/addressbok/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/by/berdysh/java_course/addressbok/tests/ContactCreationTest.java
@@ -2,9 +2,13 @@ package by.berdysh.java_course.addressbok.tests;
 
 import by.berdysh.java_course.addressbok.model.ContactData;
 import by.berdysh.java_course.addressbok.model.Contacts;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -12,17 +16,20 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTest extends TestBase {
 
 
-	@Test
-	public void testContactCreation() throws Exception {
+	@DataProvider
+	public Iterator<Object[]> validContacts() {
+		List<Object[]> list = new ArrayList<Object[]>();
+		list.add(new Object[]{new ContactData().withFirstName("FirstName1").withLastName("LastName1").withMobile("12345").withEmail("test1@test.com").withGroup("test1")});
+		list.add(new Object[]{new ContactData().withFirstName("FirstName2").withLastName("LastName2").withMobile("12345").withEmail("test2@test.com").withGroup("test2")});
+		list.add(new Object[]{new ContactData().withFirstName("FirstName3").withLastName("LastName3").withMobile("12345").withEmail("test3@test.com").withGroup("test3")});
+		return list.iterator();
+	}
+
+	@Test(dataProvider = "validContacts")
+	public void testContactCreation(ContactData contact) throws Exception {
 		app.goTo().contactPage();
 		Contacts before = app.contact().all();
 		File photo = new File("src/test/resources/photo.jpg");
-		ContactData contact = new ContactData()
-						.withFirstName("TestName").withLastName("TestLast").withAddress("Address 5/55")
-						.withEmail("test@email.com").withEmail2("test2@email.com").withEmail3("test3@email.com")
-						.withMobile("+375(29)").withHomePhone("12 34 56").withWorkPhone("98-76-54")
-						.withGroup("test1").withPhoto(photo);
-
 		app.contact().create(contact, true);
 		app.goTo().contactPage();
 		assertThat(app.contact().count(), equalTo(before.size() + 1));
