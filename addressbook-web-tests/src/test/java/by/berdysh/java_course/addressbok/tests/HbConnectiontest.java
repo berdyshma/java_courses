fa99851482b983a4d111152850d66ece27ad142f
berdyshma <berdysh@gmail.com>
2019-05-21 15:25:23 +0300

    Hibernate 5.4.2.Final version setup

--- addressbook-web-tests/src/test/java/by/berdysh/java_course/addressbok/tests/HbConnectiontest.java
+++ addressbook-web-tests/src/test/java/by/berdysh/java_course/addressbok/tests/HbConnectiontest.java
@@ -1,7 +1,6 @@
 package by.berdysh.java_course.addressbok.tests;
 
 import by.berdysh.java_course.addressbok.model.ContactData;
-import by.berdysh.java_course.addressbok.model.GroupData;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
