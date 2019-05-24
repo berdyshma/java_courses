9001007e91cc987d407ce0e4492547f27f712358
berdyshma <berdysh@gmail.com>
2019-05-24 14:00:30 +0300

    New module added for Mantis tests

---
+++ mantis-tests/src/test/java/by/berdysh/java_course/mantis/tests/TestBase.java
@@ -0,0 +1,33 @@
+package by.berdysh.java_course.mantis.tests;
+
+import by.berdysh.java_course_mantis.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.io.IOException;
+
+public class TestBase {
+
+	public static ApplicationManager app;
+
+	static {
+		try {
+			app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	@BeforeSuite(alwaysRun = true)
+	public void setUp() throws Exception {
+		app.init();
+	}
+
+	@AfterSuite(alwaysRun = true)
+	public void tearDown() throws Exception {
+		app.stop();
+	}
+
+}
