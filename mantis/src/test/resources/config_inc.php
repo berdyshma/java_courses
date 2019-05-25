fd3c11c2bbf81b6257bb0997a11fbcc8d95f7aee
berdyshma <berdysh@gmail.com>
2019-05-25 15:46:18 +0300

    FTP

---
+++ mantis/src/test/resources/config_inc.php
@@ -0,0 +1,8 @@
+<?php
+$g_hostname = 'localhost';
+$g_db_type = 'mysql';
+$g_db_name = 'bugtracker';
+$g_db_username = 'root';
+$g_db_password = '';
+$g_crypto_master_salt = '6qvN5DibNVXlhjeP7VrIqyfPUgaHZ14DMWklGKsriU8=';
+$g_signup_use_captcha = OFF;
