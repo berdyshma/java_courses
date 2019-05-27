4de98cbd8d2aa60c4017bdcdd0dc5e4bc9f25a83
berdyshma <berdysh@gmail.com>
2019-05-27 11:49:09 +0300

    Login Tests

--- mantis/src/test/java/by/berdysh/java_course/mantis/appmanager/HttpSession.java
+++ mantis/src/test/java/by/berdysh/java_course/mantis/appmanager/HttpSession.java
@@ -35,7 +35,7 @@ public class HttpSession {
 		post.setEntity(new UrlEncodedFormEntity(params));
 		CloseableHttpResponse response = httpclient.execute(post);
 		String body = getTextFrom(response);
-		return body.contains(String.format("<span class\"italic\">%s</span>", username));
+		return body.contains(String.format("<span class=\"italic\">%s</span>", username));
 	}
 	private String getTextFrom(CloseableHttpResponse response) throws IOException{
 		try{
@@ -48,7 +48,7 @@ public class HttpSession {
 		HttpGet get = new HttpGet (app.getProperty("web.baseUrl") + "/login.php");
 		CloseableHttpResponse response = httpclient.execute(get);
 		String body = getTextFrom(response);
-		return body.contains(String.format("<span class\"italic\">%s</span>", username));
+		return body.contains(String.format("<span class=\"italic\">%s</span>", username));
 
 
 	}
