f62724386c77c1dcd7e5a5d2214d58333814a1c1
berdyshma <berdysh@gmail.com>
2019-10-18 20:23:40 +0300

    Infrastructure for Java and first test

--- addressbook-web-tests/src/test/java/by/berdysh/java_course/addressbok/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/by/berdysh/java_course/addressbok/appmanager/ApplicationManager.java
@@ -2,6 +2,7 @@ package by.berdysh.java_course.addressbok.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -51,6 +52,7 @@ public class ApplicationManager {
 		} else {
 			DesiredCapabilities capabilities = new DesiredCapabilities();
 			capabilities.setBrowserName(browser);
+			capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win10")));
 			wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
 		}
 
---
+++ selenium/src/test/java/by/berdysh/java_course/MyFirstTest.java
@@ -0,0 +1,37 @@
+package by.berdysh.java_course;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest {
+	private WebDriver driver;
+	private WebDriverWait wait;
+
+	@Before
+	public void start(){
+		driver = new ChromeDriver();
+		wait = new WebDriverWait (driver, 10);
+
+	}
+
+	@Test
+	public void MyFirstTest(){
+		driver.get("http://www.google.com");
+		driver.findElement(By.name("q")).sendKeys("webdriver");
+		
+	}
+
+	@After
+	public void stop(){
+		driver.quit();
+		driver = null;
+	}
+
+}
