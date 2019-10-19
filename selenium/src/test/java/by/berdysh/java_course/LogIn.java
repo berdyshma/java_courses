3b419701b428060e334b8371996e500eae9e8ecc
berdyshma <berdysh@gmail.com>
2019-10-19 21:07:56 +0300

    Different browsers and capabilities

--- selenium/src/test/java/by/berdysh/java_course/LogIn.java
+++ selenium/src/test/java/by/berdysh/java_course/LogIn.java
@@ -5,8 +5,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
@@ -15,9 +15,16 @@ public class LogIn {
 	private WebDriver driver;
 	private WebDriverWait wait;
 
+
+
 	@Before
 	public void start(){
-		driver = new ChromeDriver();
+		//driver = new ChromeDriver();
+		//driver = new FirefoxDriver();
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setCapability(InternetExplorerDriver.IGNORE_ZOOM_SETTING, true);
+		caps.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+	  driver = new InternetExplorerDriver(caps);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		wait = new WebDriverWait (driver, 10);
 
